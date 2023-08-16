f0566a1824add2f5fc6217199de0e3cb01d8dd12
larisgomes <lgomes@87labs.com>
2023-08-16 16:20:27 -0300

    feat(cancel): add method delayed cancel

--- lib/chargify_wrapper/resources/subscription.rb
+++ lib/chargify_wrapper/resources/subscription.rb
@@ -1,6 +1,9 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
 module ChargifyWrapper
   class Subscription < Base
+    def delayed_cancel(reason = nil, code = nil)
+      self.post(:delayed_cancel, nil, {subscription: {cancellation_message: reason, reason_code: code}}.to_json)
+    end
   end
 end
