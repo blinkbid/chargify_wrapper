96ebe2bd56bb3217c40266243afa4201c2479aae
larisgomes <lgomes@87labs.com>
2023-09-01 17:46:22 -0300

    fix(subscription): code adjust

--- lib/chargify_wrapper/resources/subscription.rb
+++ lib/chargify_wrapper/resources/subscription.rb
@@ -5,12 +5,13 @@ module ChargifyWrapper
     self.include_root_in_json = true
 
     def delayed_cancel(attrs = {})
-      post(:delayed_cancel, nil, {
-        subscription: {
+      post(
+        :delayed_cancel, nil,
+        {
           cancellation_message: attrs[:reason],
           reason_code: attrs[:code]
-        }
-      }.to_xml)
+        }.to_xml(root: :subscription)
+      )
     end
   end
 end
