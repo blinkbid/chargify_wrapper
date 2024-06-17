06f58a1a47c576f5079349820876b47db0f8d29a
waitoair <luana_saraiva2@hotmail.com>
2024-06-17 14:52:52 -0300

    feat(subscription-spec): adjust the tests for the change_default_payment_profile method

--- spec/resources/subscription_spec.rb
+++ spec/resources/subscription_spec.rb
@@ -39,79 +39,55 @@ RSpec.describe ChargifyWrapper::Subscription do
   end
 
   describe "#change_default_payment_profile", :vcr do
-    let(:subscription) { described_class.find(76354749) }
+    subject(:change_payment_profile) { subscription.change_default_payment_profile(payment_profile) }
+
+    let(:payment_profile) do
+      ChargifyWrapper::PaymentProfile.create({
+        payment_profile: {
+          first_name: "Joao",
+          last_name: "Das Neves",
+          last_four: "3858",
+          card_type: "visa",
+          expiration_month: 11,
+          expiration_year: 2034,
+          customer_id: 80219272,
+          current_vault: "bogus",
+          vault_token: "1",
+          billing_address: "123 Main St.",
+          billing_city: "Boston",
+          billing_state: "MA",
+          billing_zip: "02120",
+          billing_country: "US",
+          billing_address_2: "Addd2",
+          payment_type: "credit_card"
+        }
+      })
+    end
 
     context "when payment profile is valid" do
-      let(:payment_profile) do
-        ChargifyWrapper::PaymentProfile.create({
-          payment_profile: {
-            first_name: "Joao",
-            last_name: "Das Neves",
-            last_four: "3858",
-            card_type: "visa",
-            expiration_month: 11,
-            expiration_year: 2034,
-            customer_id: 80219272,
-            current_vault: "bogus",
-            vault_token: "1",
-            billing_address: "123 Main St.",
-            billing_city: "Boston",
-            billing_state: "MA",
-            billing_zip: "02120",
-            billing_country: "US",
-            billing_address_2: "Addd2",
-            payment_type: "credit_card"
-          }
-        })
-      end
+      let(:subscription) { described_class.find(76354749) }
 
       it "returns http status 200" do
-        expect(subscription.change_default_payment_profile(payment_profile)).to be_a(Net::HTTPOK)
+        expect(change_payment_profile).to be_a(Net::HTTPOK)
       end
 
       it "changes the default payment profile" do
-        response = subscription.change_default_payment_profile(payment_profile)
-
-        parsed_response = JSON.parse(response.body)
+        parsed_response = JSON.parse(change_payment_profile.body)
         expect(parsed_response["payment_profile"]["id"]).to eq(payment_profile.id)
       end
     end
 
     context "when subscription is not found" do
-      let(:invalid_subscription) { described_class.new(id: 12345678) }
-
-      let(:payment_profile) do
-        ChargifyWrapper::PaymentProfile.create({
-          payment_profile: {
-            first_name: "Joao",
-            last_name: "Das Neves",
-            last_four: "3858",
-            card_type: "visa",
-            expiration_month: 11,
-            expiration_year: 2034,
-            customer_id: 80219272,
-            current_vault: "bogus",
-            vault_token: "1",
-            billing_address: "123 Main St.",
-            billing_city: "Boston",
-            billing_state: "MA",
-            billing_zip: "02120",
-            billing_country: "US",
-            billing_address_2: "Addd2",
-            payment_type: "credit_card"
-          }
-        })
-      end
+      let(:subscription) { described_class.new(id: 12345678) }
 
       it "raises an ActiveResource::ResourceNotFound error" do
-        expect {
-          invalid_subscription.change_default_payment_profile(payment_profile)
-        }.to raise_error(ActiveResource::ResourceNotFound)
+        expect { change_payment_profile }.to raise_error(ActiveResource::ResourceNotFound)
       end
     end
 
     context "when payment profile is not found" do
-      let(:non_existent_payment_profile) do
+      let(:subscription) { described_class.find(76354749) }
+      let(:payment_profile) do
         ChargifyWrapper::PaymentProfile.new({
           id: 12345678,
           subscription_id: subscription.id
@@ -119,17 +95,7 @@ RSpec.describe ChargifyWrapper::Subscription do
       end
 
       it "raises an ActiveResource::ResourceNotFound error" do
-        expect {
-          subscription.change_default_payment_profile(non_existent_payment_profile)
-        }.to raise_error(ActiveResource::ResourceNotFound)
-      end
-    end
-
-    context "when changing the default payment profile fails" do
-      let(:invalid_payment_profile) do
-      end
-
-      it "raises a 422 Unprocessable Entity error" do
+        expect { change_payment_profile }.to raise_error(ActiveResource::ResourceNotFound)
       end
     end
   end
