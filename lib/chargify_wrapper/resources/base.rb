039bab47d20ec5365cddd4145b5ba2fe408d7625
Reynaldo Scardinho <r.scardinho@gmail.com>
2024-07-10 16:30:12 -0300

    feat(base): add method to handle 422 api errors

--- lib/chargify_wrapper/resources/base.rb
+++ lib/chargify_wrapper/resources/base.rb
@@ -3,5 +3,18 @@
 module ChargifyWrapper
   class Base < ActiveResource::Base
     self.include_root_in_json = true
+
+    private
+
+    def handle_errors(&block)
+      yield if block_given?
+    rescue ActiveResource::ResourceInvalid => e
+      body = JSON.parse(e.response.body)
+      errors = body.fetch(self.class.element_name, 'errors')
+
+      self.errors.from_array(errors)
+
+      self
+    end
   end
 end
