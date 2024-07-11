783bdbf04bf29a8c64c3344c3ac7c81abed71635
Reynaldo Scardinho <r.scardinho@gmail.com>
2024-07-11 18:21:26 -0300

    feat(rubocop): adjust rubocop offenses

--- lib/chargify_wrapper/resources/coupon.rb
+++ lib/chargify_wrapper/resources/coupon.rb
@@ -4,9 +4,9 @@ module ChargifyWrapper
   class Coupon < Base
     class << self
       def validate(code, product_family_id: nil)
-        params = { code: code }
+        params = {code: code}
 
-        params.merge!(product_family_id: product_family_id) if product_family_id
+        params[:product_family_id] = product_family_id if product_family_id
 
         get(:validate, params)
       end
--- spec/resources/coupon_spec.rb
+++ spec/resources/coupon_spec.rb
@@ -4,24 +4,24 @@ require "spec_helper"
 
 RSpec.describe ChargifyWrapper::Coupon do
   describe "#validate", :vcr do
-    subject { described_class.validate(coupon) }
+    subject(:validate_coupon) { described_class.validate(coupon) }
 
     context "when coupon is existent and valid" do
       let(:coupon) { "GENERALCOUPON" }
 
-      it { expect(subject).to be_present }
+      it { expect(validate_coupon).to be_present }
     end
 
     context "when coupon is not existent" do
       let(:coupon) { "ANYCOUPON" }
 
-      it { expect { subject }.to raise_error(ActiveResource::ResourceNotFound) }
+      it { expect { validate_coupon }.to raise_error(ActiveResource::ResourceNotFound) }
     end
 
     context "when coupon is existent but not valid" do
       let(:coupon) { "25RDJMSNET" }
 
-      it { expect { subject }.to raise_error(ActiveResource::ResourceNotFound) }
+      it { expect { validate_coupon }.to raise_error(ActiveResource::ResourceNotFound) }
     end
   end
 end
