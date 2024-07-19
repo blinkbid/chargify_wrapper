170dd32f6e506f05881b34d7c8cd63b215d25dbd
waitoair <luana_saraiva2@hotmail.com>
2024-07-19 13:31:13 -0300

    feat(version): release version 0.5.1

--- spec/chargify_wrapper_spec.rb
+++ spec/chargify_wrapper_spec.rb
@@ -4,6 +4,6 @@ require "spec_helper"
 
 RSpec.describe ChargifyWrapper do
   it "has a version number" do
-    expect(ChargifyWrapper::VERSION).to eql("0.5.0")
+    expect(ChargifyWrapper::VERSION).to eql("0.5.1")
   end
 end
